ff78f027f337a87892ea550cebae5bf4adfac13d
engelisart <144841027+engelisart@users.noreply.github.com>
2023-11-10 16:35:56 +0000

    Deploying to gh-pages from @ engelisart/goit-js-hw-09@abe16932d6c08406c29411a849997a2e7f7be62e 🚀

---
+++ 01-color-switcher.81c760b6.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");document.querySelector("[data-start]");t.addEventListener("click",(function(){d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0})),e.disabled=!0;let d=null;
+//# sourceMappingURL=01-color-switcher.81c760b6.js.map
