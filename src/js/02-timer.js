ce0802b1c2fdc06fecd3351f2542199a03845cef
engelisart <144841027+engelisart@users.noreply.github.com>
2023-11-11 16:16:18 +0100

    updated hw 2-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,20 +1,89 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.css';
-import { startTimeButton } from './refs';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { startButton, startTimeButton } from './refs';
 
 startTimeButton.addEventListener('click', onStartTimeBtn);
 
 startTimeButton.disabled = true;
 
-function onStartTimeBtn() {
+const timer = {
+  intervalId: null,
+  deadline: new Date(2023, 10, 12),
+  refs: {
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+  },
 
+  start() {
+    this.intervalId = setInterval(() => {
+      const diff = this.deadline - Date.now();
+
+      if (diff < 0) {
+        this.stop();
+
+        return;
+      }
+
+      let { days, hours, minutes, seconds } = this.getTimeComponents(diff);
+
+      this.refs.days.textContent = this.pad(days);
+      this.refs.hours.textContent = this.pad(hours);
+      this.refs.minutes.textContent = this.pad(minutes);
+      this.refs.seconds.textContent = this.pad(seconds);
+
+      console.log(this.getTimeComponents(diff));
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+  },
+
+  getTimeComponents(diff) {
+    const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const hours = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const minutes = Math.floor(diff / 1000 / 60) % 60;
+    const seconds = Math.floor(diff / 1000) % 60;
+
+    return {
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  },
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  },
+};
+
+function onStartTimeBtn() {
+  timer.start();
 }
 
 flatpickr('.data-time', {
-  minDate: Date.now(),
   enableTime: true,
-  dateFormat: "Y-m-d H:i",
+  dateFormat: 'Y-m-d H:i',
   onChange: function (selectedDates, dateStr, instance) {
-    console.log(dateStr);
+    now = Date.now();
+
+    selected = selectedDates[0];
+
+    if (now >= selected) {
+      Notify.failure('Please choose a date in the future');
+
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+
+      timer.deadline = selected;
+    }
   },
 });
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+function createPromise(delay, position) {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      
+      if (Math.random() > 0.3) {
+        resolve({ delay, position });
+      } else {
+        reject({ delay, position });
+      }
+    }, delay);
+  });
+}
+
+const formElem = document.querySelector('.form');
+
+formElem.addEventListener('submit', onFormElem);
+
+function onFormElem(event) {
+  event.preventDefault();
+
+  let firstdelay = +event.currentTarget.elements.delay.value;
+  let delay = +event.currentTarget.elements.step.value;
+  let amount = +event.currentTarget.elements.amount.value;
+
+  for (let i = 0; i < amount; i++) {
+    let newdelay = firstdelay + delay * i;
+
+    createPromise(newdelay, i + 1)
+      .then(({ delay, position }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay} ms`);
+      })
+      .catch(({ delay, position }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay} ms`);
+      });
   }
 }
